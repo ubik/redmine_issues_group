3a878839ace19671e199536867050f8f82edb0df
Ubik <andrew_chaika@inbox.ru>
2009-04-30 01:21:21 +0400

    - removed parent field from bulk edit - added issue context menu item "edit parent"

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -3,11 +3,33 @@ require_dependency 'issues_controller'
 
 class IssuesController < ApplicationController
 	skip_before_filter :authorize, :only => [:autocomplete_for_parent]
+  before_filter :find_issues, :only => [:bulk_edit, :move, :destroy, :parent_edit]
   def autocomplete_for_parent
     @issues = Issue.find(:all, :conditions => ["LOWER(subject) LIKE ? OR id LIKE ?", "%#{params[:text]}%", "%#{params[:text]}%"],
                               :limit => 10,
                               :order => 'id ASC').uniq
     render :layout => false
+  end
+  def parent_edit
+    if request.post?
+      if params[:parent]
+        i = Issue.find_by_id(params[:parent]) rescue nil unless params[:parent].empty? 
+        if @issues.include?(i)
+          flash[:error] = l(:notice_failed_to_update)
+        else
+          @issues.each do |issue|
+            if i.nil?
+              issue.move_to_root()
+            else
+              issue.move_to_child_of(i) 
+            end
+          end
+          flash[:notice] = l(:notice_successful_update) unless @issues.empty?
+        end
+      end
+      redirect_to(params[:back_to] || {:controller => 'issues', :action => 'index', :project_id => @project})
+      return
+    end
   end
   private
   def retrieve_query_with_groupby
--- init.rb
+++ init.rb
@@ -2,7 +2,6 @@ require 'redmine'
 require 'query'
 require_dependency 'issues_helper_patch'
 require 'issue_relation_patch'
-require 'issues_parent_hook'
 
 Redmine::Plugin.register :redmine_issues_group do
   name 'Redmine Issues Group plugin'
@@ -10,4 +9,14 @@ Redmine::Plugin.register :redmine_issues_group do
   description 'This is a issue group plugin for Redmine'
   version '0.0.7'
   requires_redmine :version_or_higher => '0.8.0' 
+  project_module :issue_tracking do
+    permission :edit_parent, {:issues => [:parent_edit]}
+  end
 end
+
+class RedmineIssuesParentListener < Redmine::Hook::ViewListener
+  render_on :view_issues_context_menu_end, :inline => 
+   "<li><%= context_menu_link l(:button_parent_edit), {:controller => 'issues', :action => 'parent_edit', :ids => @issues.collect(&:id)}," +
+                           " :class => 'icon-parent-edit' %></li>"
+  render_on :view_layouts_base_html_head, :inline => "<%= stylesheet_link_tag 'stylesheet', :plugin => 'redmine_issues_group' %>"
+end 
